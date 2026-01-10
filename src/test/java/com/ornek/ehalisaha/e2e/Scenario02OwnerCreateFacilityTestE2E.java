30f1b052215515785b58e6055bc9386320e92044
RuhanKarimov <ruhankarimovwork@gmail.com>
2026-01-10 09:49:27 +0300

    Update Scenario02OwnerCreateFacilityTestE2E.java

--- src/test/java/com/ornek/ehalisaha/e2e/Scenario02OwnerCreateFacilityTestE2E.java
+++ src/test/java/com/ornek/ehalisaha/e2e/Scenario02OwnerCreateFacilityTestE2E.java
@@ -41,6 +41,6 @@ public class Scenario02OwnerCreateFacilityTestE2E extends BaseE2ETestE2E {
         System.out.println("Facility Name: " + byId("ownerFacilitySel").getText());
         System.out.println("Facility Name: " + byId("ownerFacilitySel").getText());
         System.out.println("Facility Name: " + byId("ownerFacilitySel").getText());
-        assertTrue(byId("ownerFacilitySel").getText().contains("Arena"));
+        assertTrue(byId("ownerFacilitySel").getText().contains("rena"));
     }
 }
