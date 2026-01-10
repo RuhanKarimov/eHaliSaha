99f92be129e6fa9789b9abcf601d8b062e23de4e
RuhanKarimov <ruhankarimovwork@gmail.com>
2026-01-11 00:47:55 +0300

    Revert "Update BaseE2ETestE2E.java"
    This reverts commit a6c86c61c5b07c1d69486a05f8742df8d24aafa0.

--- src/test/java/com/ornek/ehalisaha/e2e/BaseE2ETestE2E.java
+++ src/test/java/com/ornek/ehalisaha/e2e/BaseE2ETestE2E.java
@@ -500,35 +500,10 @@ public abstract class BaseE2ETestE2E {
         // 6) select
         selectByContainsText(selLoc, name);
 
-        // Bazı UI'larda select ile seçim change event’i bazen tetiklenmeyebiliyor.
-        // Garantileyelim:
-        try {
-            WebElement selEl = driver.findElement(selLoc);
-            ((JavascriptExecutor) driver).executeScript(
-                    "arguments[0].dispatchEvent(new Event('change', {bubbles:true}));", selEl
-            );
-        } catch (Exception ignored) {}
-
-        tryRefreshAll(); // UI.refreshAll varsa iyi
-
-        // Pitch dropdown "yüklendi mi?" diye bekle (pitch var mı diye değil!)
-        By pitchSel = By.id("ownerPitchSel");
-        WebDriverWait w = new WebDriverWait(driver, Duration.ofSeconds(30));
-        w.until(d -> {
-            try {
-                Select ps = new Select(d.findElement(pitchSel));
-                return ps.getOptions().size() >= 1; // placeholder bile olsa "loaded"
-            } catch (Exception e) {
-                return false;
-            }
-        });
-
         // 7) Eğer UI hata gösteriyorsa, net patlatalım
-        String outAfter = safeText(outLoc);
-        if (isErrorLike(outAfter)) {
-            fail("Facility akışı hata verdi. ownerOut=" + outAfter);
+        if (isErrorLike(outNow)) {
+            fail("Facility oluşturma hata verdi. ownerOut=" + outNow);
         }
-
     }
 
     protected void setSlotsDayAndSave() {
@@ -548,9 +523,6 @@ public abstract class BaseE2ETestE2E {
 
         String out = safeText(outLoc);
         if (isErrorLike(out)) fail("Slot kaydetme hata verdi. ownerOut=" + out);
-
-        tryRefreshAll();
-
     }
 
     protected void ensurePitchExists(String pitchName) {
@@ -569,7 +541,6 @@ public abstract class BaseE2ETestE2E {
             btn = By.xpath("//button[normalize-space(.)='Ekle']");
         }
         clickSmart(btn);
-        tryRefreshAll();
 
         WebDriverWait w = new WebDriverWait(driver, Duration.ofSeconds(45));
         w.pollingEvery(Duration.ofMillis(300));
