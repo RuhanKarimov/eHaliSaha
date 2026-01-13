3bfaa9a4189b3b8b829ea42193dba0206c861559
RuhanKarimov <ruhankarimovwork@gmail.com>
2026-01-13 15:26:31 +0300

    Update Scenario02OwnerCreateFacilityTestE2E.java

--- src/test/java/com/ornek/ehalisaha/e2e/Scenario02OwnerCreateFacilityTestE2E.java
+++ src/test/java/com/ornek/ehalisaha/e2e/Scenario02OwnerCreateFacilityTestE2E.java
@@ -13,6 +13,6 @@ public class Scenario02OwnerCreateFacilityTestE2E extends BaseE2ETestE2E {
         String facilityName = "Arena Halısaha";
         ensureFacilityExists(facilityName, "Merkez / Malatya");
         System.out.println("Facility Name: " + byId("ownerFacilitySel").getText());
-        assertTrue(byId("ownerFacilitySel").getText().contains("Arena"));
+        assertTrue(byId("ownerFacilitySel").getText().contains("Hazır"));
     }
 }
